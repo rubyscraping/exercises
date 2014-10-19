f119022c83e46acaffb645d9e2f628565f7b0980
George Drummond <georgedrummond@gmail.com>
2014-10-19 01:15:56 +0100

    Slight formatting change

--- requests/user_agent.rb
+++ requests/user_agent.rb
@@ -3,7 +3,9 @@ require 'net/http'
 
 url = URI.parse('http://ifconfig.me/ua')
 http = Net::HTTP.start(url.host, url.port)
-request = Net::HTTP::Get.new(url.path, {'User-Agent' => 'ExampleBot/1.0'})
+request = Net::HTTP::Get.new(url.path, {
+  'User-Agent' => 'ExampleBot/1.0'
+})
 response = http.request(request)
 
 puts response.body
